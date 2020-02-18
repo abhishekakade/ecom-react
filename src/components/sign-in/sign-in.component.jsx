4eb7c569971da8c5bc988140f07987e0c30fc0ec
Abhishek K <abhishekakade@gmail.com>
2020-02-18 23:12:23 +0530

    feat: sign out & sign in both working

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -32,7 +32,7 @@ class SignIn extends React.Component {
 
   render() {
     return (
-      <div className="sign-in">
+      <form className="sign-in">
         <h2>I already have an account!</h2>
         <span>Sign in with your email and password.</span>
 
@@ -54,13 +54,14 @@ class SignIn extends React.Component {
             label="password"
             required
           />
-
-          <CustomButton type="submit">Sign In</CustomButton>
-          <CustomButton onClick={signInWithGoogle}>
-            Sign in with Google
-          </CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit">Sign In</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div>
         </form>
-      </div>
+      </form>
     );
   }
 }
