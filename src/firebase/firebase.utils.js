7ab29ccc56343779b2f089c40232d0a280fcd8a0
Abhishek K <abhishekakade@gmail.com>
2020-02-23 23:22:51 +0530

    fix: create and save new user if user doesnt exist

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,6 +14,43 @@ const config = {
   measurementId: "G-E7558ZW453"
 };
 
+// takes the user auth object we got from authentication and store it in database
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  // firestore returns 2 types of objects - references & snapshots.
+  // They can be either Document or Collection.
+  // firestore will always return these objects even if nothing exists at the query endpoint.
+
+  // We use DocumentReference object to perform CRUD operations - set(), get(), update(), delete()
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+  console.log("snapshot", snapShot);
+  // exists property from this snapshot console log will tell whether any data exists on requested query
+
+  // so if snapshot doesnt exist, this will create new user and set data on that date
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("Error creating user: ", error.message);
+    }
+  }
+
+  // return userRef so we can use it to do more if needed
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
